73438a57347b9f2f608b3d38867a787662993254
sandeep <munagalasandeepreddy@gamail.com>
2024-04-21 09:46:58 +0200

    code

--- frontend.sh
+++ frontend.sh
@@ -1,19 +1,19 @@
 # installing nginx server for web frame
 echo -e "\e[33mInstalling nginx server\e[0m"
 
-dnf install nginx -y
+dnf install nginx -y >/tmp/roboshop.log
 
 
 
-systemctl start nginx
+systemctl start nginx >/tmp/roboshop.log
 
 echo -e "\e[33mRemoving old app contente[0m"
 rm -rf /usr/share/nginx/html/*
 
 echo -e "\e[33mextract front end content\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip >/tmp/roboshop.log
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip >/tmp/roboshop.log
 # we need to copy config file
-systemctl enable nginx
-systemctl restart nginx
+systemctl enable nginx >/tmp/roboshop.log
+systemctl restart nginx >/tmp/roboshop.log
