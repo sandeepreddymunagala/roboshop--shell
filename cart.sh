fce22ac931f2d664ad2667e91d1ed1fbb842d8c3
sandeep <munagalasandeepreddy@gamail.com>
2024-04-22 12:23:51 +0200

    code

--- cart.sh
+++ cart.sh
@@ -1,18 +1,16 @@
-echo -e "\e[33mconfiguring nodejs repose[0m"
+echo -e "\e[33m disabling nodejs repos \e[0m"
 dnf module disable nodejs -y &>> /tmp/roboshop.log
 
-echo -e "\e[33mconfiguring nodejs repose[0m"
-dnf module enable nodejs:18 -y &>> /tmp/roboshop.log
-
 echo -e "\e[33m configuring nodejs repos \e[0m"
+dnf module enable nodejs:18 -y &>> /tmp/roboshop.log
 dnf install nodejs -y &>> /tmp/roboshop.log
 
-echo -e "\e[33m add application user \e[0m"
-useradd roboshop &>> /tmp/roboshop.log
+echo -e "\e[33m add application cart \e[0m"
+cartadd roboshop &>> /tmp/roboshop.log
 rm -rf /app
 mkdir /app
 
-echo -e "\e[33mc downloading application context \e[0m"
+echo -e "\e[33m downloading application context \e[0m"
 curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip &>> /tmp/roboshop.log
 cd /app
 
@@ -23,9 +21,10 @@ echo -e "\e[33m install nodejs dependencies \e[0m"
 npm install &>> /tmp/roboshop.log
 
 echo -e "\e[33m update systemd service \e[0m"
-cp cart.service /etc/systemd/system/catalouge.service &>> /tmp/roboshop.log
+cp /root/roboshop--shell/cart.service /etc/systemd/system/cart.service &>> /tmp/roboshop.log
 
 echo -e "\e[33m start cart service \e[0m"
 systemctl daemon-reload &>> /tmp/roboshop.log
 systemctl enable cart &>> /tmp/roboshop.log
 systemctl start cart &>> /tmp/roboshop.log
+
