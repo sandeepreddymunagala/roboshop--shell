6838f9baf7cabac2119afcefc7fb10dcd689876f
sandeep <munagalasandeepreddy@gamail.com>
2024-04-22 11:24:04 +0200

    code

--- catalogue.sh
+++ catalogue.sh
@@ -3,8 +3,6 @@ dnf module disable nodejs -y &>> /tmp/roboshop.log
 
 echo -e "\e[33m configuring nodejs repos \e[0m"
 dnf module enable nodejs:18 -y &>> /tmp/roboshop.log
-
-echo -e "\e[33m configuring nodejs repos \e[0m"
 dnf install nodejs -y &>> /tmp/roboshop.log
 
 echo -e "\e[33m add application user \e[0m"
@@ -23,7 +21,7 @@ echo -e "\e[33m install nodejs dependencies \e[0m"
 npm install &>> /tmp/roboshop.log
 
 echo -e "\e[33m update systemd service \e[0m"
-cp /root/roboshop--shell/catalogue.service /etc/systemd/system/catalouge.service &>> /tmp/roboshop.log
+cp /root/roboshop--shell/catalogue.service /etc/systemd/system/catalogue.service &>> /tmp/roboshop.log
 
 echo -e "\e[33m start catalogue service \e[0m"
 systemctl daemon-reload &>> /tmp/roboshop.log
